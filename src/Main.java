766d523e447279c4341a4b93d1fc9a9cd501ab8f
Anatoliy <bugrov.anatoliy@yandex.ru>
2022-06-22 21:46:25 +1000

    Create Hello

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         String b = "Hello Bro!!";
+        String c = "Hello Man!!";
         SayHello sayHello = new SayHello(b);
-        sayHello.sayHello(b);
+        sayHello.sayHello(c);
         System.out.println(sayHello.a);
 
     }
